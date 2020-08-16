7485e19654bd485bd0e47dd7f256695ff599b271
Bwuckster <bwuck@bwuck.com>
2020-08-16 01:50:37 -0700

    Awesome.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -260,7 +260,7 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts.
    dotspacemacs-default-font '(("Cascadia Code" ;; PL variant doesn't work everywhere.
                                 :size 13.0
-                                :weight normal
+                                :weight regular
                                 :width normal)
                                ("Fira Code"
                                 :size 15.0
