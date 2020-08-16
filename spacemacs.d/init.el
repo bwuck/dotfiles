d8146b813ca873bc9c63381e01d34189900f774c
Bwuckster <bwuck@bwuck.com>
2020-08-16 00:48:06 -0700

    Document why not PL.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -258,7 +258,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
 
    ;; Default font or prioritized list of fonts.
-   dotspacemacs-default-font '(("Cascadia Code"
+   dotspacemacs-default-font '(("Cascadia Code" ;; PL variant doesn't work everywhere.
                                 :size 13.0
                                 :weight normal
                                 :width normal)
