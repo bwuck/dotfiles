f709e2847f6a2ec3e025712591d8e55d865881ed
Bwuckster <bwuck@bwuck.com>
2020-12-08 17:24:00 -0800

    Alacritty and fish configuration.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -31,6 +31,7 @@ if status --is-interactive
         $HOME/.local/bin \
         $HOME/.cargo/bin \
         $HOME/.yarn/bin \
+	$HOME/flutter/bin \
         $HOME/go/bin \
         /snap/bin \
         /mingw64/bin \
